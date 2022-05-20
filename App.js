f7d9f1d9b56611efd00b99edcc8d420274cb08a9
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-20 00:45:06 -0700

    add: submit btn, delete btn, strike funct

--- App.js
+++ App.js
@@ -1,2 +1,54 @@
-let x = null;
-let y = "hi";
+//status: when completed button clicked, removes all items in list. no cross out animation
+
+const submitButton = document.getElementById("add-item").addEventListener('click', addItem);
+const item = document.getElementById("list");
+const removeButton = document.getElementById("delete").addEventListener('click', removeListItem);
+const removeItem = document.getElementById('list');
+const insertStrike = document.getElementById('list-item').addEventListener('click', crossOutItem);
+
+function addItem(e){
+    e.preventDefault();
+    //get value
+    let getItem = document.getElementById("item").value;
+    //create new li el
+    let newListItem = document.createElement("li");
+    //add class
+    newListItem.className = 'list-item';
+    console.log(newListItem);
+    //append node
+    
+
+    const newRemoveBtn = document.createElement("button");
+    newRemoveBtn.className = 'delete';
+    newRemoveBtn.appendChild(document.createTextNode("Complete"));
+    
+    newListItem.appendChild(document.createTextNode(getItem));
+    item.appendChild(newRemoveBtn);
+    item.appendChild(newListItem);
+   
+}
+
+function removeListItem(e){
+    if (e.target.className.contains('delete')){
+        console.log(2)
+        if (confirm("Remove Item From List?")){
+            let removeListItem = e.target.parentElement;
+            item.removeChild(removeItem);
+        }
+    }
+
+    let itemCompleted = document.getElementById("list-item");
+    itemCompleted.removeChild(item);
+
+
+
+}
+
+function crossOutItem(){
+    let crossOut = insertStrike.strike();
+
+}
+
+function searchItems(){
+
+}
---
+++ investigations.js
@@ -0,0 +1,22 @@
+// function factorial(num){
+//     //base case
+//     if (num == 0){
+//         console.log(num);
+//         return 1
+//     }
+//     console.log(num);
+//     return num - factorial(num - 1);
+// }
+// factorial(4);
+//------------------
+
+// function ternaryFactorial(num){
+//     //base case
+//     console.log(num)
+//     let factorial = num == 0
+//         ? 1
+//         : num - ternaryFactorial(num - 1);
+//     return factorial;
+// }
+// ternaryFactorial(8);
+// console.log("this element is not a string");
