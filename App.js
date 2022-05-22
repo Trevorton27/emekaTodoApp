93ac8cc60b6b3aac61377b681f406673152e1c80
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-21 17:31:04 -0700

    edit: vars and scope

--- App.js
+++ App.js
@@ -1,28 +1,24 @@
 //status: when completed button clicked, removes all items in list. no cross out animation
 
 const submitButton = document.getElementById("add-item").addEventListener('click', addItem);
-const item = document.getElementById("list");
-const removeButton = document.getElementById("delete").addEventListener('click', removeListItem);
-const removeItem = document.getElementById('list');
 const insertStrike = document.getElementById('list-item').addEventListener('click', crossOutItem);
 
 function addItem(e){
     e.preventDefault();
     //get value
-    let getItem = document.getElementById("item").value;
+    let getItem = document.getElementById("item");
     //create new li el
     let newListItem = document.createElement("li");
     //add class
     newListItem.className = 'list-item';
     console.log(newListItem);
-    //append node
     
+    const todoList = document.getElementById("list");
 
     const newRemoveBtn = document.createElement("button");
     newRemoveBtn.className = 'delete';
-    newRemoveBtn.appendChild(document.createTextNode("Complete"));
-    
-    newListItem.appendChild(document.createTextNode(getItem));
+
+    newListItem.textContent = getItem.value;
     item.appendChild(newRemoveBtn);
     item.appendChild(newListItem);
    
