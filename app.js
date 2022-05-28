ee0eaf31fab402310c1954b098c05127cb135c23
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-27 23:33:16 -0700

    edit: css checkbox

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-//status: when completed button clicked, removes all items in list. no cross out animation
-
 const submitButton = document.getElementById("add-item")
 .addEventListener('click', addItem);
-
-
 
 function addItem(e){
     e.preventDefault();
@@ -15,30 +11,31 @@ function addItem(e){
     newListItem.className = 'u-list';
     const todoList = document.getElementById("list");
 
+    //create checkboxes
+    const strikeThrough = document.createElement("input");
+    strikeThrough.setAttribute("type", "checkbox");
+    strikeThrough.className = "checkboxes";
+    
+    //invoke line-through for each new item
+    s.addEventListener('click', () => crossOutItem(newListItem));
 
-    let s = document.createElement("input");
-    s.setAttribute("tpye", "checkbox");
-    s.className = "checkboxes"
-    s.addEventListener('click', crossOutItem);
-
-
+    //create remove btn for each new item
     const newRemoveBtn = document.createElement("button")
     newRemoveBtn.className = 'delete'
     newRemoveBtn.textContent = "Remove";
     newRemoveBtn.addEventListener("click", removeListItem);
+
+    //append elements
     newListItem.textContent = getItem.value;
     todoList.appendChild(s);
     todoList.appendChild(newListItem);
     newListItem.appendChild(newRemoveBtn);
 
-   
-
-
+    
 }
 
 function removeListItem(e){
     e.preventDefault();
-
     let confirmRemove = confirm("Remove Item From List?");
     if (confirmRemove == true){
         e.target.parentElement.remove();
@@ -46,25 +43,17 @@ function removeListItem(e){
         console.log("cancelled");
     }
 }
-function crossOutItem(e){
-    e.preventDefault();
-    //e.target.parentElement.crossOutItem();
-    //e.target.parentElement.style.textDecoration = 'line-through';
-    //"\u0336" + e.target.parentElement;
-    //crossOut.className = "strike-class";
-    //strike.innerText = "test";
-    //strike.parentElement.style.setProperty("text-decoration", "line-through");
-    console.log(1)
-    // let crossOut = document.createElement("checkbox");
-    // crossOut.className = 'check';
 
-
-
-
+function crossOutItem(element){
+    //call CSS styling for line-through
+    element.classList.toggle('check');
 }
-//Do this ONLY if you have time to spare and can quickly get it done
-// function searchItems(){
+
+//---possible additions--
+//clear text field
+//todoList.addEventListener('click', clearTextField);
+// function clearTextField(e){
+//     e.preventDefault();
+//     const textField = document.getElementById("item");
+//     textField.value = "";
 // }
-function testGitHub(){
-    //test
-}
